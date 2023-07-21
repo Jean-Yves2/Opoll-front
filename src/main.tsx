5929a806afc5cd27924fa247987e5c51a9296de0
Titouan-Goubet <titouangoubet@hotmail.fr>
2023-07-21 15:07:14 +0200

    little fix styles files

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store';
 import App from './components/App/App';
 import './styles/reset.css';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
