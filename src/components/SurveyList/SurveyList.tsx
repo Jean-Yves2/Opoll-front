8c4bcfd2e4e739ff8cce9c8f62a6a23f7dd41556
Jean-Yves2 <sarakajean1@gmail.com>
2023-07-24 16:01:31 +0200

    add : role admin

--- src/components/SurveyList/SurveyList.tsx
+++ src/components/SurveyList/SurveyList.tsx
@@ -2,6 +2,9 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Button } from '@mui/material';
 import './SurveyList.scss';
+import Cookies from 'js-cookie';
+import DeleteIcon from '@mui/icons-material/Delete';
+import DriveFileRenameOutlineRoundedIcon from '@mui/icons-material/DriveFileRenameOutlineRounded';
 
 interface Survey {
   data: Array<{
@@ -16,6 +19,13 @@ interface Survey {
     updated_at: string;
   }>;
 }
+export interface userProfile {
+  username: string;
+  email: string;
+  avatar: string;
+  isAdmin: boolean;
+}
+
 function SurveyList() {
   const [surveys, setSurveys] = useState<Survey>({
     data: [
@@ -32,31 +42,63 @@ function SurveyList() {
       },
     ],
   });
-  
-  const getSurveys = () => {
+
+  const [admin, setAdmin] = useState<boolean>(false);
+
+  const token = Cookies.get('token');
+
+  const getProfile = () => {
+    const data = '';
+
+    const config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:3000/@me',
+      headers: {
+        Authorization: token,
+      },
+      data: data,
+    };
     axios
-      .get('http://localhost:3000/survey')
+      .request(config)
       .then((response) => {
-        console.log(response);
-        setSurveys(response);
+        console.log(JSON.stringify(response.data));
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+        setAdmin(response.data.isAdmin);
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
+  const getSurveys = () => {
+    axios
+      .get('http://localhost:3000/survey')
+      .then((response) => {
+        //console.log(response);
+        setSurveys(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  getProfile();
+
   useEffect(() => {
     getSurveys();
   }, []);
 
   return (
-    <div  className='containerSurveyList'>
-      <h1>Liste des enquêtes</h1>
-      
-      <ul >
+    <div className="containerSurveyList">
+      <h1><br /><br />
+        Liste des enquêtes
+      </h1>
+
+      <ul>
         {surveys.data.map((survey) => (
-          <li  key={survey.id} >
-            <Button variant="contained"
+          <li key={survey.id}>
+            <Button
+              variant="contained"
               size="large"
               color="primary"
               style={{
@@ -64,7 +106,17 @@ function SurveyList() {
                 height: '6.25rem',
                 display: 'flex',
                 alignItems: 'center',
-              }}>
+                justifyContent: 'space-between',
+              }}
+              endIcon={
+                admin ? (
+                  <div>
+                    <DriveFileRenameOutlineRoundedIcon />
+                    <DeleteIcon />
+                  </div>
+                ) : null
+              }
+            >
               {survey.title}
             </Button>
           </li>
